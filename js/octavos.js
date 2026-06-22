0ab50b7707f51134ae3e91b1276aa9da09ccd1de
calebsanchez17 <calebsanchez9012@gmail.com>
2026-06-22 11:12:10 -0500

    Se agrego

--- js/octavos.js
+++ js/octavos.js
@@ -5,7 +5,7 @@ const tournamentData = {
         { name: "V9 ESPORT", points: 0 }, { name: "BR ESPORT", points: 0 },
         { name: "ESPORTS GX", points: 0 }, { name: "FAMILY E-SPORT", points: 0 },
         { name: "RED RIOT GAMG", points: 0 }, { name: "CLAN A7", points: 0 },
-        { name: "CUPO", points: 0 }, { name: "CUPO", points: 0 },
+        { name: "LW ESPORT", points: 0 }, { name: "CUPO", points: 0 },
         { name: "CUPO", points: 0 }, { name: "CUPO", points: 0 }
     ],
     2: [
@@ -13,7 +13,7 @@ const tournamentData = {
         { name: "GSX ESPORT", points: 0 }, { name: "ØNEFEELING FEM", points: 0 },
         { name: "DELTA ESPORT DIVISIÓN", points: 0 }, { name: "VIRTUS STRIKE ESPORTS", points: 0 },
         { name: "TEAM CRAZY RABBITS", points: 0 }, { name: "TEAM AFA 03", points: 0 },
-        { name: "CUPO", points: 0 }, { name: "CUPO", points: 0 },
+        { name: "CLAN TUCU OFICIAL", points: 0 }, { name: "CUPO", points: 0 },
         { name: "CUPO", points: 0 }, { name: "CUPO", points: 0 }
     ],
     3: [
@@ -21,7 +21,7 @@ const tournamentData = {
         { name: "VFT LEGIÓES", points: 0 }, { name: "SAVAGE ACD ESPORT", points: 0 },
         { name: "CLAN XHILEE 65", points: 0 }, { name: "KETZA WARRIORS", points: 0 },
         { name: "CLAN VISIONBULLS", points: 0 }, { name: "ESPORT GROUND ZERO 9", points: 0 },
-        { name: "CUPO", points: 0 }, { name: "CUPO", points: 0 },
+        { name: "CLAN RFW ESPORTS", points: 0 }, { name: "CUPO", points: 0 },
         { name: "CUPO", points: 0 }, { name: "CUPO", points: 0 }
     ],
     4: [
@@ -29,14 +29,14 @@ const tournamentData = {
         { name: "DC ESPORT RED", points: 0 }, { name: "LTX E-SPORTS", points: 0 },
         { name: "ALPHA GAMING ESPORT", points: 0 }, { name: "32 ESPORT", points: 0 },
         { name: "VIOLAᵛᶫCLAN", points: 0 }, { name: "NOVA REVELLION ESPORTS", points: 0 },
-        { name: "CUPO", points: 0 }, { name: "CUPO", points: 0 },
+        { name: "LX SPORT", points: 0 }, { name: "CUPO", points: 0 },
         { name: "CUPO", points: 0 }, { name: "CUPO", points: 0 }
     ],
     5: [
         { name: "LOS VAGOS", points: 0 }, { name: "RED RIOT GANG ESPORT", points: 0 },
         { name: "F7 E-SPORT", points: 0 }, { name: "TEAM WACHINGTON", points: 0 },
         { name: "DC ESPORT BLACK", points: 0 }, { name: "BIOHAZARD ESPORTS", points: 0 },
-        { name: "CLAN LEVELING UP", points: 0 }, { name: "CUPO", points: 0 },
+        { name: "CLAN LEVELING UP", points: 0 }, { name: "ESPORT HLP", points: 0 },
         { name: "CUPO", points: 0 }, { name: "CUPO", points: 0 },
         { name: "CUPO", points: 0 }, { name: "CUPO", points: 0 }
     ],
@@ -44,7 +44,7 @@ const tournamentData = {
         { name: "DRAGÓN Z", points: 0 }, { name: "CLAN KMK GAMING", points: 0 },
         { name: "FURY E-SPORT", points: 0 }, { name: "CLAN LEGIONMURSI", points: 0 },
         { name: "SAVAGE ESPORT", points: 0 }, { name: "CLAN TS BÉLICOS", points: 0 },
-        { name: "CLAN WONKRU", points: 0 }, { name: "CUPO", points: 0 },
+        { name: "CLAN WONKRU", points: 0 }, { name: "BLΛCK FURY", points: 0 },
         { name: "CUPO", points: 0 }, { name: "CUPO", points: 0 },
         { name: "CUPO", points: 0 }, { name: "CUPO", points: 0 }
     ],
@@ -52,7 +52,7 @@ const tournamentData = {
         { name: "TEAM KING", points: 0 }, { name: "CLAN CARTEL 23!", points: 0 },
         { name: "CLAN DARK BLOOD", points: 0 }, { name: "URU TEAM", points: 0 },
         { name: "DARK KING", points: 0 }, { name: "TEAM X7 FIELES", points: 0 },
-        { name: "DC ESPORT RED", points: 0 }, { name: "CUPO", points: 0 },
+        { name: "DC ESPORT RED", points: 0 }, { name: "CLAN SLNT〆KLRS", points: 0 },
         { name: "CUPO", points: 0 }, { name: "CUPO", points: 0 },
         { name: "CUPO", points: 0 }, { name: "CUPO", points: 0 }
     ],
@@ -60,7 +60,7 @@ const tournamentData = {
         { name: "MONKEY SQUAD", points: 0 }, { name: "GHOST DEMONDS", points: 0 },
         { name: "CLAN WE ARE", points: 0 }, { name: "TEAM SAVAGE", points: 0 },
         { name: "TEAM 04", points: 0 }, { name: "CLAN W$N", points: 0 },
-        { name: "CUPO", points: 0 }, { name: "CUPO", points: 0 },
+        { name: "TEAM SANTOSGANG", points: 0 }, { name: "CUPO", points: 0 },
         { name: "DC ESPORT BLACK", points: 0 }, { name: "CUPO", points: 0 },
         { name: "CUPO", points: 0 }, { name: "CUPO", points: 0 }
     ]
