880a61295efbf0a35fe9579ea620a6f509bd725b
calebsanchez17 <calebsanchez9012@gmail.com>
2026-07-18 23:36:24 -0500

    Fase Semi

--- js/semifinal.js
+++ js/semifinal.js
@@ -2,36 +2,36 @@
 // DATOS: TABLA GENERAL (TODO EN 0 PARA LLENAR)
 // ==========================================
 const semifinalTeams = [
-    { name: "ESPORT HLP", flag: "🇵🇪", logo: "logos/hlp.png", maps: 0, booyah: 0, ptsPos: 0, elims: 0, ptsExtra: 0 },
-    { name: "SAIYANS ESPORT", flag: "🇨🇱", logo: "logos/saiyans.png", maps: 0, booyah: 0, ptsPos: 0, elims: 0, ptsExtra: 0 },
-    { name: "TEAM 777", flag: "🇦🇷", logo: "logos/777.png", maps: 0, booyah: 0, ptsPos: 0, elims: 0, ptsExtra: 0 },
-    { name: "AG STORM ESPORT", flag: "🇺🇾", logo: "logos/ag.png", maps: 0, booyah: 0, ptsPos: 0, elims: 0, ptsExtra: 0 },
-    { name: "V9 ESPORT", flag: "🇨🇱", logo: "logos/nueve.png", maps: 0, booyah: 0, ptsPos: 0, elims: 0, ptsExtra: 0 },
-    { name: "ESPORT ZENITH", flag: "🇨🇱", logo: "logos/z7.png", maps: 0, booyah: 0, ptsPos: 0, elims: 0, ptsExtra: 0 },
-    { name: "SAMURAI", flag: "🇦🇷", logo: "logos/samurai.png", maps: 0, booyah: 0, ptsPos: 0, elims: 0, ptsExtra: 0 },
-    { name: "DC ESPORT RED", flag: "🇵🇪", logo: "logos/dc.png", maps: 0, booyah: 0, ptsPos: 0, elims: 0, ptsExtra: 0 },
-    { name: "BR ESPORT", flag: "🇵🇪", logo: "logos/br.png", maps: 0, booyah: 0, ptsPos: 0, elims: 0, ptsExtra: 0 },
-    { name: "F7 E-SPORT", flag: "🇦🇷", logo: "logos/f7.png", maps: 0, booyah: 0, ptsPos: 0, elims: 0, ptsExtra: 0 },
-    { name: "RANK FLOW ESPORT", flag: "🇵🇪", logo: "logos/rf.png", maps: 0, booyah: 0, ptsPos: 0, elims: 0, ptsExtra: 0 },
-    { name: "LOSP40", flag: "🇦🇷", logo: "logos/40.png", maps: 0, booyah: 0, ptsPos: 0, elims: 0, ptsExtra: 0 },
-    { name: "V9 ESPORT", flag: "🇨🇱", logo: "logos/nueve.png", maps: 0, booyah: 0, ptsPos: 0, elims: 0, ptsExtra: 0 },
-    { name: "FURIUS LYNX", flag: "🇦🇷", logo: "logos/flx.png", maps: 0, booyah: 0, ptsPos: 0, elims: 0, ptsExtra: 0 },
-    { name: "TEAM CRAZY", flag: "🇦🇷", logo: "logos/crazy.png", maps: 0, booyah: 0, ptsPos: 0, elims: 0, ptsExtra: 0 },
-    { name: "TEAM 04", flag: "🇧🇷", logo: "logos/04.png", maps: 0, booyah: 0, ptsPos: 0, elims: 0, ptsExtra: 0 },
-    { name: "CLAN CARTEL 23!", flag: "🇦🇷", logo: "logos/23.png", maps: 0, booyah: 0, ptsPos: 0, elims: 0, ptsExtra: 0 },
-    { name: "GHOST DEMONDS", flag: "🇦🇷", logo: "logos/gd.png", maps: 0, booyah: 0, ptsPos: 0, elims: 0, ptsExtra: 0 }
+    { name: "ESPORT HLP", flag: "🇵🇪", logo: "logos/hlp.png", maps: 4, booyah: 0, ptsPos: 17, elims: 18, ptsExtra: 0 },
+    { name: "SAIYANS ESPORT", flag: "🇨🇱", logo: "logos/saiyans.png", maps: 4, booyah: 1, ptsPos: 26, elims: 70, ptsExtra: 0 },
+    { name: "TEAM 777", flag: "🇦🇷", logo: "logos/777.png", maps: 4, booyah: 0, ptsPos: 19, elims: 39, ptsExtra: 0 },
+    { name: "AG STORM ESPORT", flag: "🇺🇾", logo: "logos/ag.png", maps: 4, booyah: 2, ptsPos: 34, elims: 66, ptsExtra: 5 },
+    { name: "V9 ESPORT", flag: "🇨🇱", logo: "logos/nueve.png", maps: 4, booyah: 0, ptsPos: 11, elims: 15, ptsExtra: 0 },
+    { name: "ESPORT ZENITH", flag: "🇨🇱", logo: "logos/z7.png", maps: 4, booyah: 0, ptsPos: 18, elims: 18, ptsExtra: 0 },
+    { name: "SAMURAI", flag: "🇦🇷", logo: "logos/samurai.png", maps: 4, booyah: 0, ptsPos: 18, elims: 17, ptsExtra: 0 },
+    { name: "DC ESPORT RED", flag: "🇵🇪", logo: "logos/dc.png", maps: 4, booyah: 0, ptsPos: 8, elims: 13, ptsExtra: 5 },
+    { name: "BR ESPORT", flag: "🇵🇪", logo: "logos/br.png", maps: 4, booyah: 1, ptsPos: 31, elims: 99, ptsExtra: 0 },
+    { name: "F7 E-SPORT", flag: "🇦🇷", logo: "logos/f7.png", maps: 4, booyah: 0, ptsPos: 3, elims: 5, ptsExtra: 5 },
+    { name: "RANK FLOW ESPORT", flag: "🇵🇪", logo: "logos/rf.png", maps: 4, booyah: 1, ptsPos: 29, elims: 41, ptsExtra: 5 },
+    { name: "LOSP40", flag: "🇦🇷", logo: "logos/40.png", maps: 4, booyah: 0, ptsPos: 18, elims: 28, ptsExtra: 0 },
+    { name: "V9 ESPORT", flag: "🇨🇱", logo: "logos/nueve.png", maps: 4, booyah: 0, ptsPos: 25, elims: 23, ptsExtra: 0 },
+    { name: "FURIUS LYNX", flag: "🇦🇷", logo: "logos/flx.png", maps: 4, booyah: 0, ptsPos: 9, elims: 15, ptsExtra: 0 },
+    { name: "TEAM CRAZY RABBITS", flag: "🇦🇷", logo: "logos/crazy.png", maps: 4, booyah: 1, ptsPos: 26, elims: 22, ptsExtra: 0 },
+    { name: "TEAM 04", flag: "🇧🇷", logo: "logos/04.png", maps: 4, booyah: 0, ptsPos: 12, elims: 24, ptsExtra: 0 },
+    { name: "CLAN CARTEL 23!", flag: "🇦🇷", logo: "logos/23.png", maps: 4, booyah: 0, ptsPos: 14, elims: 17, ptsExtra: 0 },
+    { name: "GHOST DEMONDS", flag: "🇦🇷", logo: "logos/gd.png", maps: 4, booyah: 0, ptsPos: 23, elims: 23, ptsExtra: 5 }
 ];
 
 // ==========================================
 // DATOS: BOOYAH RECAP (TODO EN 0 PARA LLENAR)
 // ==========================================
 const mapRecapData = [
-    { map: "MAPA 1 - BERMUDA", bg: "mapas/bermuda.jpg", team: "", flag: "🏳️", elims: 0, pts: 0, logo: "logos/.png" },
-    { map: "MAPA 2 - PURGATORIO", bg: "mapas/purgatorio.jpg", team: "", flag: "🏳️", elims: 0, pts: 0, logo: "logos/.png" },
-    { map: "MAPA 3 - BERMUDA", bg: "mapas/bermuda.jpg", team: "", flag: "🏳️", elims: 0, pts: 0, logo: "logos/.png" },
-    { map: "MAPA 4 - KALAHARI", bg: "mapas/kalahari.jpg", team: "", flag: "🏳️", elims: 0, pts: 0, logo: "logos/.png" },
-    { map: "MAPA 5 - BERMUDA", bg: "mapas/bermuda.jpg", team: "", flag: "🏳️", elims: 0, pts: 0, logo: "logos/.png" },
-    { map: "MAPA 6 - NEXTERRA", bg: "mapas/nexterra.jpg", team: "", flag: "🏳️", elims: 0, pts: 0, logo: "logos/.png" }
+    { map: "MAPA 1 - BERMUDA", bg: "mapas/bermuda.jpg", team: "RANK FLOW ESPORT", flag: "🇵🇪", elims: 11, pts: 23, logo: "logos/rf.png" },
+    { map: "MAPA 2 - PURGATORIO", bg: "mapas/purgatorio.jpg", team: "BR ESPORT", flag: "🇵🇪", elims: 35, pts: 47, logo: "logos/br.png" },
+    { map: "MAPA 3 - BERMUDA", bg: "mapas/bermuda.jpg", team: "AG STORM ESPORT", flag: "🇺🇾", elims: 17, pts: 29, logo: "logos/ag.png" },
+    { map: "MAPA 4 - KALAHARI", bg: "mapas/kalahari.jpg", team: "TEAM CRAZY RABBITS", flag: "🇦🇷", elims: 8, pts: 20, logo: "logos/crazy.png" },
+    { map: "MAPA 5 - BERMUDA", bg: "mapas/bermuda.jpg", team: "SAIYANS ESPORT", flag: "🇨🇱", elims: 24, pts: 36, logo: "logos/saiyans.png" },
+    { map: "MAPA 6 - NEXTERRA", bg: "mapas/nexterra.jpg", team: "AG STORM ESPORT", flag: "🇺🇾", elims: 28, pts: 40, logo: "logos/ag.png" }
 ];
 
 window.onload = () => {
